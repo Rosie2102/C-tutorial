8e84f83fe23102c2d9c516d3b9df8faaececb130
Rosie2102 <rosienguyen2002@icloud.com>
2023-04-17 20:22:17 +0930

    added

---
+++ dataType.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main(){
+    int a=5;
+    string b = "3";
+    string c = "3.14";
+    bool d = true;
+    cout << a+stoi(b)<<endl;
+    
+}
