33f2c98d57c91bb7a1cbf4c352bea7bd8266ef3b
Rosie2102 <rosienguyen2002@icloud.com>
2023-04-17 20:23:46 +0930

    added

---
+++ typeCasting.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main(){
+    int numerator = 40;
+    int denominator = 25;
+    int number = 5;
+    cout<< boolalpha <<(bool)number << endl;
+    cout<< numerator / denominator <<endl;
+    cout<< (double)numerator / denominator<<endl;
+    printf("%d \n", numerator/denominator);
+ 
+}
